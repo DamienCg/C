8a90c125dcc55f31d13068d4c208e84089afaacd
Damien Ciagola <damien.ciagola@studenti.unipd.it>
2017-08-10 11:02:25 +0200

    Update QuickSort[Da completare.cpp

--- Algorithm/QuickSort[Da completare.cpp
+++ Algorithm/QuickSort[Da completare.cpp
@@ -1,44 +1,38 @@
+// Example program
 #include <iostream>
+#include <string>
 using namespace std;
-/*
 
- */
- 
-int Partition(int*A,int p,int r){
-	int x=A[r-1];
-	int i=p-1;
-	
-	for(int j=p; j<r-1; j++){
-		if(A[j]<=x){
-			i++;
-			int temp=A[i];
-			A[i]=A[j];
-			A[j]=temp;
-		}
-	}
-	
-	int temp=A[i+1];
-	A[i]=A[r];
-	A[r]=temp;
-	return i+1;
+int Partition(int *A,int p,int r){
+    int x=A[r];
+    int i=p-1;
+    
+    for(int j=p; j<r-1; j++){
+        if (A[j]<x){
+         i++;
+         int temp=A[i];
+         A[i]=A[j];
+         A[j]=temp;
+        }
+    }
+     int temp=A[i+1];
+         A[i+1]=A[r];
+         A[r]=temp;
+         
+         return i+1;
 }
 
-
-void QuickSort(int*A,int p,int r){
-	if(p<r){
-	 int q=Partition(A,p,r);
-		QuickSort(A,p,q-1);
-		QuickSort(A,q+1,r);
-	}
+void Quicksort (int*A,int p,int r){  // Complessità massima      
+ if(p < r){          
+ int q= Partition(A,p,r);         
+ Quicksort(A,p,q-1);        
+ Quicksort(A,q+1,r);
+ }
 }
 
-main(){
-int A[10]={33,1,0,-15,1,54,3,12,99,-1};
-int n=10;
-
-QuickSort(A,0,n);
-
-for(int i=0; i<10; i++)
-cout<<A[i]<<" ";
-
+int main(){
+    int A[10]={3,4,1,7,9,8,5,2,10,6};
+    Quicksort(A,0,9);
+    for(int i=0; i<10; i++)
+    cout<<A[i]<<" ";
 }
